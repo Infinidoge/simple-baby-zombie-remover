1825fb480faefff558f24b756d87a8b42922b54b
Infinidoge <infinidoge@inx.moe>
2024-08-07 21:30:04 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Baby Zombie Remover
+
+Removes Baby Zombies. That's the entire mod.
+
+Overrides `ZombieEntity`'s `shouldBeBaby` method to always return `false`.
---
+++ src/main/java/moe/inx/simple_baby_zombie_remover/mixin/ZombieEntityMixin.java
@@ -0,0 +1,16 @@
+package moe.inx.simple_baby_zombie_remover.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.mob.ZombieEntity;
+
+@Mixin(ZombieEntity.class)
+public abstract class ZombieEntityMixin {
+	@Inject(method = "/method_29936/", at = @At(value = "RETURN"), cancellable = true, remap = false)
+	private static void simple_baby_zombie_remover$shouldBeBaby(CallbackInfoReturnable<Boolean> ci) {
+		ci.setReturnValue(false);
+	}
+}
